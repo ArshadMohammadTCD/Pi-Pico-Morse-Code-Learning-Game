571771701a9f40b4be4332ed857ad9060044485b
merrinsj <merrinsj@tcd.ie>
2022-03-30 00:03:53 +0100

    basic setup work for buttons

--- assign02.c
+++ assign02.c
@@ -1,10 +1,36 @@
-// Must declare the main assembly entry point before use.
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+// Declare the main assembly code entry point.
 void main_asm();
-
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
+// Initialise a GPIO pin – see SDK for detail on gpio_init()
+void asm_gpio_init(uint pin) {
+ gpio_init(pin);
+}
+// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+void asm_gpio_set_dir(uint pin, bool out) {
+ gpio_set_dir(pin, out);
+}
+// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+bool asm_gpio_get(uint pin) {
+ return gpio_get(pin);
+}
+// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+void asm_gpio_put(uint pin, bool value) {
+    gpio_put(pin, value);
+}
+// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq_fall(uint pin) {
+     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
+}
+// Enable rising-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq_rise(uint pin) {
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
+}
+// Main entry point of the application
 int main() {
-    main_asm();
-    return(0);
+ stdio_init_all(); // Initialise all basic IO
+ printf("Assignment #2...\n"); // Basic print to console
+ main_asm(); // Jump into the ASM code
+ return 0; // Application return code
 }
