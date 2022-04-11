f2c53b9466589919385b9502337f605e45016247
merrinsj <merrinsj@tcd.ie>
2022-04-11 13:57:59 +0100

    Tom and Jamie co-piloting wrote more logic in the .S file and created the readMorseString function in the .c file

--- assign02.c
+++ assign02.c
@@ -3,30 +3,48 @@
 #include "hardware/gpio.h"
 // Declare the main assembly code entry point.
 void main_asm();
+
+
 // Initialise a GPIO pin – see SDK for detail on gpio_init()
 void asm_gpio_init(uint pin) {
  gpio_init(pin);
 }
+
+
 // Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
 void asm_gpio_set_dir(uint pin, bool out) {
  gpio_set_dir(pin, out);
 }
+
+
 // Get the value of a GPIO pin – see SDK for detail on gpio_get()
 bool asm_gpio_get(uint pin) {
  return gpio_get(pin);
 }
+
+
 // Set the value of a GPIO pin – see SDK for detail on gpio_put()
 void asm_gpio_put(uint pin, bool value) {
     gpio_put(pin, value);
 }
+
+
 // Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
 void asm_gpio_set_irq_fall(uint pin) {
      gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
 }
+
+
 // Enable rising-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
 void asm_gpio_set_irq_rise(uint pin) {
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
 }
+
+void readMorseString( char * morseString)
+{
+
+}
+
 // Main entry point of the application
 int main() {
  stdio_init_all(); // Initialise all basic IO
